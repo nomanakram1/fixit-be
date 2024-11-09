5c9682cc2504dcfe0ebc9ace859e11823d348b6a
nomanakram1 <mr.noman143@gmail.com>
2024-11-09 23:17:32 +0500

    Update NestJS configuration for Heroku deployment

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // Set up Swagger
   const config = new DocumentBuilder()
