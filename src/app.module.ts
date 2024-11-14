857e847d4916e7a9c1e0de5874ab6ac2f2bed662
Azmat Khan <azmatkhan0470727@gmail.com>
2024-11-14 16:00:09 +0500

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { UserDetailsModule } from './userDetails/userDetails.module';
 import { RolesModule } from './roles/roles.module';
 import { UserToRolesModule } from './userToRoles/userToRoles.module';
 import { SubscriptionPlanModule } from './subscriptionPlan/subscriptionplan.module';
+import { UserSubscriptionPlanModule } from './userSubscriptionPlan/usersubscriptionplan.module';
 
 @Module({
   imports: [
@@ -40,6 +41,7 @@ import { SubscriptionPlanModule } from './subscriptionPlan/subscriptionplan.modu
     RolesModule,
     UserToRolesModule,
     SubscriptionPlanModule,
+    UserSubscriptionPlanModule
   ],
   controllers: [AppController],
   providers: [AppService],
